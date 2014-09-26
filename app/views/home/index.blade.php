9515517896d474273bd03dddfbac55c8b209d27a
nouse4aname14 <cdbco16@yahoo.com>
2014-09-25 21:49:25 -0700

    Update index.blade.php

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="jumbotron">
         <h1>{{ $title }}</h1>
-        <p class="lead">Find the latest weather forecast for each city by selecting a city from the drop down menu and clicking search.</p>
+        <p class="lead">Find the latest weather forecast for each city by selecting a city from the drop down menu and clicking search. Hold down the Ctrl (windows) / Command (Mac) button to select multiple options.</p>
         <div class="city-selector-container">
             <select multiple id="city-selector" class="form-control input-lg">
                 @foreach ($cities as $key => $city)
